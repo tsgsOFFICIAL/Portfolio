ac41da16425f4f2368a3f9099d5ce2d96feb72a2
Marcus Jensen <mj@jf-data.com>
2021-12-09 15:07:13 +0100

    nav links now work / change "active" state

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 // document.documentElement.style.setProperty('--root-level-variable', 'value');
+const navLinks = document.querySelectorAll("body nav ul li a");
+
+navLinks.forEach((navLink) => {
+    navLink.addEventListener("click", changeActiveLink, navLink);
+});
+
+function changeActiveLink(e) {
+    const target = e.target;
+
+    if (target.classList.contains("hover")) {
+
+        navLinks.forEach((navLink) => {
+            navLink.className = "underline hover from-middle";
+        });
+        target.className = "underline gradient";
+    }
+}
